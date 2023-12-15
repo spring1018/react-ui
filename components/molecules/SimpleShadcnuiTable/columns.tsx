1880dd6118401e5a2929928e1aac5e75db467eca
spring1018 <spring1018nbrandall@gmail.com>
2023-12-15 17:39:21 +0900

    Refactor

--- components/molecules/SimpleShadcnuiTable/columns.tsx
+++ components/molecules/SimpleShadcnuiTable/columns.tsx
@@ -2,8 +2,17 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 
+import { z } from "zod";
 import { DataTableColumnHeader } from "./data-table-column-header";
-import { Task } from "./schema";
+
+export const taskSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  status: z.string(),
+  priority: z.string(),
+});
+
+export type Task = z.infer<typeof taskSchema>;
 
 const columnDefs = [
   {
@@ -23,31 +32,6 @@ const columnDefs = [
     title: "Priority",
   },
 ];
-
-// const checkboxColumn: ColumnDef<Task> = {
-//   id: "select",
-//   header: ({ table }) => (
-//     <Checkbox
-//       checked={
-//         table.getIsAllPageRowsSelected() ||
-//         (table.getIsSomePageRowsSelected() && "indeterminate")
-//       }
-//       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-//       aria-label="Select all"
-//       className="translate-y-[2px]"
-//     />
-//   ),
-//   cell: ({ row }) => (
-//     <Checkbox
-//       checked={row.getIsSelected()}
-//       onCheckedChange={(value) => row.toggleSelected(!!value)}
-//       aria-label="Select row"
-//       className="translate-y-[2px]"
-//     />
-//   ),
-//   enableSorting: false,
-//   enableHiding: false,
-// };
 
 export const columns: ColumnDef<Task>[] = columnDefs.map((columnDef) => {
   return {
