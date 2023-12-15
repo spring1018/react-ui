e87d0a45f29dbfad7bdff345a30f1e1e615662c1
spring1018 <spring1018nbrandall@gmail.com>
2023-12-15 11:20:16 +0900

    Fix storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,11 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    // "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/**/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
@@ -17,6 +19,16 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@/components": path.resolve(__dirname, "../components"),
+        "@/lib": path.resolve(__dirname, "../lib"),
+      };
+    }
+    return config;
   },
 };
 export default config;
