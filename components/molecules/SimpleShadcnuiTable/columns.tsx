bbfebb8379b705300de2ea3be3834ee297a43d22
spring1018 <spring1018nbrandall@gmail.com>
2023-12-19 16:11:16 +0900

    Enable sub component in Table

--- components/molecules/SimpleShadcnuiTable/columns.tsx
+++ components/molecules/SimpleShadcnuiTable/columns.tsx
@@ -2,6 +2,8 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 
+import { CustomSelect } from "@/components/atoms/CustomSelect";
+import { Input } from "@/components/ui/input";
 import { z } from "zod";
 import { DataTableColumnHeader } from "./data-table-column-header";
 
@@ -14,7 +16,19 @@ export const taskSchema = z.object({
 
 export type Task = z.infer<typeof taskSchema>;
 
-const columnDefs = [
+type ColumnDefType = {
+  accessorKey: keyof Task;
+  title: string;
+  componentType?: "input" | "select";
+  params?: {
+    selectOptions?: {
+      value: string;
+      label: string;
+    }[];
+  }
+};
+
+const columnDefs: ColumnDefType[] = [
   {
     accessorKey: "id",
     title: "Task",
@@ -22,10 +36,28 @@ const columnDefs = [
   {
     accessorKey: "title",
     title: "Title",
+    componentType: "input",
   },
   {
     accessorKey: "status",
     title: "Status",
+    componentType: "select",
+    params: {
+      selectOptions: [
+        {
+          value: "backlog",
+          label: "Backlog",
+        },
+        {
+          value: "todo",
+          label: "To Do",
+        },
+        {
+          value: "in progress",
+          label: "In Progress",
+        },
+      ],
+    },
   },
   {
     accessorKey: "priority",
@@ -39,11 +71,35 @@ export const columns: ColumnDef<Task>[] = columnDefs.map((columnDef) => {
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title={columnDef.title} />
     ),
-    cell: ({ row }) => (
-      <div className="flex max-w-[500px] h-1 items-center">
-        {row.getValue(columnDef.accessorKey)}
-      </div>
-    ),
+    cell: ({ row }) => {
+      if (columnDef.componentType === "input") {
+        return (
+          <Input
+            type="text"
+            value={row.getValue(columnDef.accessorKey)}
+            onChange={(e) => {
+              // Handle input change if needed
+            }}
+          />
+        );
+      } else if (columnDef.componentType === "select") {
+        return (
+          <CustomSelect
+            options={columnDef.params?.selectOptions ?? []}
+            initialValue={row.getValue(columnDef.accessorKey)}
+            onChange={(value) => {
+              // Handle select change if needed
+            }}
+          />
+        );
+      }
+
+      return (
+        <div className="flex max-w-[500px] h-1 items-center">
+          {row.getValue(columnDef.accessorKey)}
+        </div>
+      );
+    },
     enableSorting: true,
     enableHiding: false,
   };
