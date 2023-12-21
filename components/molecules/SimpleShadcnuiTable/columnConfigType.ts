66cd05e530b0c273726674f7b6921ba8a4314858
spring1018 <spring1018nbrandall@gmail.com>
2023-12-21 11:36:07 +0900

    Add columuConfigType

---
+++ components/molecules/SimpleShadcnuiTable/columnConfigType.ts
@@ -0,0 +1,11 @@
+export type ColumnConfigType = {
+  accessorKey: string;
+  title: string;
+  componentType?: "input" | "select";
+  params?: {
+    selectOptions?: {
+      value: string;
+      label: string;
+    }[];
+  }
+};
