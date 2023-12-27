071f9e876fcb9f2a98483ce6f14aab8c03ffec20
spring1018 <spring1018nbrandall@gmail.com>
2023-12-27 10:42:59 +0900

    Refine SimpleShadcnuiTable

--- components/molecules/SimpleShadcnuiTable/SimpleShadcnuiTable.stories.tsx
+++ components/molecules/SimpleShadcnuiTable/SimpleShadcnuiTable.stories.tsx
@@ -1,32 +1,34 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { SimpleShadcnuiTable } from '.';
+import { SimpleShadcnuiTable } from ".";
 import { columnConfigs } from "./columnConfigs";
 import { columns } from "./columns";
-type T = typeof SimpleShadcnuiTable
+type T = typeof SimpleShadcnuiTable;
 
 const data = [
   {
-    "id": "TASK-8782",
-    "title": "You can't compress the program without quantifying the open-source SSD pixel!",
-    "status": "in progress",
-    "label": "documentation",
-    "priority": "medium"
+    id: "TASK-8782",
+    title:
+      "You can't compress the program without quantifying the open-source SSD pixel!",
+    status: "in progress",
+    label: "documentation",
+    priority: "medium",
   },
   {
-    "id": "TASK-7878",
-    "title": "Try to calculate the EXE feed, maybe it will index the multi-byte pixel!",
-    "status": "backlog",
-    "label": "documentation",
-    "priority": "medium"
+    id: "TASK-7878",
+    title:
+      "Try to calculate the EXE feed, maybe it will index the multi-byte pixel!",
+    status: "backlog",
+    label: "documentation",
+    priority: "medium",
   },
   {
-    "id": "TASK-7839",
-    "title": "We need to bypass the neural TCP card!",
-    "status": "todo",
-    "label": "bug",
-    "priority": "high"
+    id: "TASK-7839",
+    title: "We need to bypass the neural TCP card!",
+    status: "todo",
+    label: "bug",
+    priority: "high",
   },
-]
+];
 
 export default {
   title: "molecules/SimpleShadcnuiTable",
@@ -37,4 +39,44 @@ export default {
   },
 } satisfies Meta<T>;
 
-export const Default: StoryObj<T> = {};
+export const Default: StoryObj<T> = {
+  args: {
+    columns: columns([
+      {
+        accessorKey: "id",
+        title: "Task",
+      },
+      {
+        accessorKey: "title",
+        title: "Title",
+        componentType: "input",
+      },
+      {
+        accessorKey: "status",
+        title: "Status",
+        componentType: "select",
+        params: {
+          selectOptions: [
+            {
+              value: "backlog",
+              label: "Backlog",
+            },
+            {
+              value: "todo",
+              label: "To Do",
+            },
+            {
+              value: "in progress",
+              label: "In Progress",
+            },
+          ],
+        },
+      },
+      {
+        accessorKey: "priority",
+        title: "Priority",
+      },
+    ]),
+    data,
+  },
+};
--- components/molecules/SimpleShadcnuiTable/columns.tsx
+++ components/molecules/SimpleShadcnuiTable/columns.tsx
@@ -4,45 +4,46 @@ import { ColumnDef } from "@tanstack/react-table";
 
 import { CustomSelect } from "@/components/atoms/CustomSelect";
 import { Input } from "@/components/ui/input";
-import { columnDefs } from "./columnDefs";
 import { DataTableColumnHeader } from "./data-table-column-header";
 
-export const columns: ColumnDef<any>[] = columnDefs.map((columnDef) => {
-  return {
-    ...columnDef,
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title={columnDef.title} />
-    ),
-    cell: ({ row }) => {
-      if (columnDef.componentType === "input") {
-        return (
-          <Input
-            type="text"
-            value={row.getValue(columnDef.accessorKey)}
-            onChange={(e) => {
-              // Handle input change if needed
-            }}
-          />
-        );
-      } else if (columnDef.componentType === "select") {
-        return (
-          <CustomSelect
-            options={columnDef.params?.selectOptions ?? []}
-            initialValue={row.getValue(columnDef.accessorKey)}
-            onChange={(value) => {
-              // Handle select change if needed
-            }}
-          />
-        );
-      }
+export const columns = (columnConfigs: ColumnDef<any>[]): any[] => {
+  return columnConfigs.map((columnConfig: any) => {
+    return {
+      ...columnConfig,
+      header: ({ column }: any) => (
+        <DataTableColumnHeader column={column} title={columnConfig.title} />
+      ),
+      cell: ({ row }: any) => {
+        if (columnConfig.componentType === "input") {
+          return (
+            <Input
+              type="text"
+              value={row.getValue(columnConfig.accessorKey)}
+              onChange={(e) => {
+                // Handle input change if needed
+              }}
+            />
+          );
+        } else if (columnConfig.componentType === "select") {
+          return (
+            <CustomSelect
+              options={columnConfig.params?.selectOptions ?? []}
+              initialValue={row.getValue(columnConfig.accessorKey)}
+              onChange={(value) => {
+                // Handle select change if needed
+              }}
+            />
+          );
+        }
 
-      return (
-        <div className="flex max-w-[500px] h-1 items-center">
-          {row.getValue(columnDef.accessorKey)}
-        </div>
-      );
-    },
-    enableSorting: true,
-    enableHiding: false,
-  };
-});
+        return (
+          <div className="flex max-w-[500px] h-1 items-center">
+            {row.getValue(columnConfig.accessorKey)}
+          </div>
+        );
+      },
+      enableSorting: true,
+      enableHiding: false,
+    };
+  });
+};
