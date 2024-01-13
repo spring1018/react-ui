4d371992488603bf462f4c0b7dbe8380a16a9467
spring1018 <spring1018nbrandall@gmail.com>
2024-01-13 16:40:27 +0900

    Tweak

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-// import Table from '@/features/posts/components/Table'
+import { TanstackEditableTable } from "@/features/tanstack/TanstackEditableTable"
 
 export default function Home() {
   return (
     <div className='px-2 py-2'>
-      {/* <Table /> */}
-      test
+      <TanstackEditableTable />
     </div>
   )
 }
