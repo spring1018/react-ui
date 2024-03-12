6f863d268bd34c765dca92009d81be3f11f88723
spring1018 <spring1018nbrandall@gmail.com>
2024-03-12 13:34:11 +0900

    Add new sheet table

---
+++ app/sample/sheet-table/columns.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { FormSheetButton } from "@/components/molecules/FormSheetButton";
+import { priorities, statuses } from "./options";
+import { Task } from "./schema";
+
+const formColumnDefs = [
+  {
+    accessorKey: "title",
+    title: "Title",
+    componentType: "input",
+  },
+  {
+    accessorKey: "status",
+    title: "Status",
+    componentType: "select",
+    params: {
+      selectOptions: statuses,
+    },
+  },
+  {
+    accessorKey: "priority",
+    title: "Priority",
+    componentType: "select",
+    params: {
+      selectOptions: priorities,
+    },
+  },
+];
+
+export const columns: ColumnDef<Task>[] = [
+  {
+    id: "button",
+    header: "",
+    cell: ({ row }) => (
+      <FormSheetButton
+        columnDefs={formColumnDefs}
+        initialValues={row.original}
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "id",
+    header: "ID",
+    cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[500px] truncate font-medium">
+            {row.getValue("title")}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status = statuses.find(
+        (status) => status.value === row.getValue("status"),
+      );
+
+      if (!status) {
+        return null;
+      }
+
+      return (
+        <div className="flex w-[100px] items-center">
+          <span>{status.label}</span>
+        </div>
+      );
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: "priority",
+    header: "Priority",
+    cell: ({ row }) => {
+      const priority = priorities.find(
+        (priority) => priority.value === row.getValue("priority"),
+      );
+
+      if (!priority) {
+        return null;
+      }
+
+      return (
+        <div className="flex items-center">
+          <span>{priority.label}</span>
+        </div>
+      );
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+];
---
+++ app/sample/sheet-table/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { ShadcnuiTable } from "@/components/organisms/ShadcnuiTable";
+import useSWR from "swr";
+import { columns } from "./columns";
+
+export default function SheetTablePage() {
+  const apiUrl = "http://localhost:3004/mock-sample";
+
+  const fetcher = (url: string): Promise<any> =>
+    fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(apiUrl, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  return (
+    <div className="px-2 py-2">
+      <ShadcnuiTable
+        columns={columns}
+        data={data}
+        // apiUrl={apiUrl}
+        // pageSizes={[5, 20, 30, 40, 50]}
+      />
+    </div>
+  );
+}
