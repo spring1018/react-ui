1205fa80e3a7e671c382b8d98dbe96aa31b0e131
spring1018 <spring1018nbrandall@gmail.com>
2024-07-17 05:13:17 +0900

    Tweak

--- components/molecules/AuthNavBar/index.tsx
+++ components/molecules/AuthNavBar/index.tsx
@@ -1,4 +1,5 @@
 import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
 import { LoginButton } from "../LoginButton";
 import { LogoutButton } from "../LogoutButton";
 import { NavBar } from "../NavBar";
@@ -10,14 +11,20 @@ type NavItem = {
 };
 
 interface AuthNavBarProps {
+  bgColor?: string;
   logo?: string;
   items: NavItem[];
   session: any;
 }
 
-export default function AuthNavBar({ logo, items, session }: AuthNavBarProps) {
+export default function AuthNavBar({
+  bgColor = "bg-white",
+  logo,
+  items,
+  session,
+}: AuthNavBarProps) {
   return (
-    <header className="bg-white sticky top-0 mx-8 z-40">
+    <header className={cn(bgColor, "sticky top-0 px-6 z-40")}>
       <div className="flex h-16 items-center justify-between py-6">
         <NavBar logo={logo} items={items} />
         {session?.user ? <LogoutButton /> : <LoginButton />}
