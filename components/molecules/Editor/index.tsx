8d15bdb2a24f934e2916d76049406612a80564b0
spring1018 <spring1018nbrandall@gmail.com>
2024-08-08 09:35:25 +0900

    Add Editor

---
+++ components/molecules/Editor/index.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Block, BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useMemo } from "react";
+
+async function saveToStorage(jsonBlocks: Block[]) {
+  console.log("Saving content:", JSON.stringify(jsonBlocks));
+  console.log("Saving content:", jsonBlocks);
+}
+
+export default function Editor({ initialContent }: { initialContent: string }) {
+  const parsedContent: PartialBlock[] | undefined = useMemo(() => {
+    return initialContent ? JSON.parse(initialContent) : undefined;
+  }, [initialContent]);
+
+  const editor = useMemo(() => {
+    return BlockNoteEditor.create({
+      initialContent: parsedContent,
+
+      domAttributes: {
+        editor: {
+          style: "font-size: 16px;padding-inline: 40px",
+        },
+      },
+    });
+  }, [parsedContent]);
+
+  if (editor === undefined) {
+    return "Loading content...";
+  }
+
+  return (
+    <div className="space-y-2">
+      <BlockNoteView className={"border"} editor={editor} />
+      <Button onClick={() => saveToStorage(editor.document)}>Save</Button>
+    </div>
+  );
+}
