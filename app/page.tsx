5f2a6da3113405baae4df3a41adf0497808dee40
spring1018 <spring1018nbrandall@gmail.com>
2024-08-19 13:20:54 +0900

    Authorize link page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,39 @@
 import Link from "next/link";
+import { getServerSession } from "./_clients/nextAuth";
 
 export default async function IndexPage() {
+  const session = await getServerSession();
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          {session?.user.name === "Admin" ? (
+            <>
+              <Link
+                href="/sample"
+                className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+              >
+                Sample
+              </Link>
+              <Link
+                href="/taxonomy"
+                className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+              >
+                Taxonomy
+              </Link>
+              <Link
+                href="/project-management"
+                className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+              >
+                Project Management
+              </Link>
+            </>
+          ) : null}
           <Link
-            href="/sample"
+            href="/strategy-dashboard"
             className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
           >
-            Sample
-          </Link>
-          <Link
-            href="/taxonomy"
-            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
-          >
-            Taxonomy
-          </Link>
-          <Link
-            href="/project-management"
-            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
-          >
-            Project Management
+            Strategy Dashboard
           </Link>
         </div>
       </section>
