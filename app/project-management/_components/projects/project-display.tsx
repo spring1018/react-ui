e25030bfbbb6b4b7d0a80995856c2ec32f89d5ee
spring1018 <spring1018nbrandall@gmail.com>
2024-08-21 09:45:18 +0900

    Tweak

--- app/project-management/_components/projects/project-display.tsx
+++ app/project-management/_components/projects/project-display.tsx
@@ -1,4 +1,5 @@
 import Timeline from "@/components/organisms/Timeline";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Project, ProjectActivity } from "../../type";
 import ProjectForm from "./project-form";
@@ -8,54 +9,37 @@ interface ProjectDisplayProps {
   activities: ProjectActivity[];
 }
 
-// const activities = [
-//   {
-//     id: 1,
-//     projectId: 1,
-//     userId: "admin",
-//     tag: "info",
-//     content: "",
-//     createdAt: new Date(),
-//   },
-//   {
-//     id: 2,
-//     projectId: 1,
-//     userId: "admin",
-//     tag: "info",
-//     content: "",
-//     createdAt: new Date(),
-//   },
-// ];
-
 export default function ProjectDisplay({
   item,
   activities,
 }: ProjectDisplayProps) {
   return (
-    <div className="space-y-2">
+    <div className="space-y-2 overflow-y-auto">
       <h2>プロジェクト詳細</h2>
-      <Tabs defaultValue="overview">
-        <TabsList>
-          <TabsTrigger value="overview">概要</TabsTrigger>
-          <TabsTrigger value="activity">活動</TabsTrigger>
-        </TabsList>
-        <TabsContent value="overview">
-          <ProjectForm defaultValues={item} key={item?.id || ""} />
-        </TabsContent>
-        <TabsContent value="activity">
-          <div className="p-2 space-y-4">
-            {activities.map((activity) => (
-              <Timeline
-                key={activity.id}
-                userName={activity.userId}
-                date={activity.createdAt}
-                tag={activity.tag}
-                content={activity.content}
-              />
-            ))}
-          </div>
-        </TabsContent>
-      </Tabs>
+      <ScrollArea className="h-[85vh]">
+        <Tabs defaultValue="overview">
+          <TabsList>
+            <TabsTrigger value="overview">概要</TabsTrigger>
+            <TabsTrigger value="activity">活動</TabsTrigger>
+          </TabsList>
+          <TabsContent value="overview">
+            <ProjectForm defaultValues={item} key={item?.id || ""} />
+          </TabsContent>
+          <TabsContent value="activity">
+            <div className="p-2 space-y-4">
+              {activities.map((activity) => (
+                <Timeline
+                  key={activity.id}
+                  userName={activity.userId}
+                  date={activity.createdAt}
+                  tag={activity.tag}
+                  content={activity.content}
+                />
+              ))}
+            </div>
+          </TabsContent>
+        </Tabs>
+      </ScrollArea>
     </div>
   );
 }
