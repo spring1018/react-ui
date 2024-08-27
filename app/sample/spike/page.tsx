1f9260901411e10bb525078d9ad844447ebe835c
spring1018 <spring1018nbrandall@gmail.com>
2024-08-27 15:03:54 +0900

    Add CircleProgress

--- app/sample/spike/page.tsx
+++ app/sample/spike/page.tsx
@@ -1,25 +1,10 @@
 "use client";
-import { MultiSelect } from "@/components/molecules/MultiSelect";
-import { useState } from "react";
+import { CircleProgress } from "@/components/molecules/CircleProgress";
 
-export default function MultiSelectPage() {
-  const options = [
-    { label: "Option 1", value: "option1" },
-    { label: "Option 2", value: "option2" },
-    { label: "Option 3", value: "option3" },
-    { label: "Option 4", value: "option4" },
-    { label: "Option 5", value: "option5" },
-  ];
-
-  const [selected, setSelected] = useState<string[]>([]);
-
+export default function Page() {
   return (
     <div className="p-4">
-      <MultiSelect
-        options={options}
-        selected={selected}
-        onChange={setSelected}
-      />
+      <CircleProgress value={30} indicatorColor="blue" backgroudColor="cyan" />
     </div>
   );
 }
