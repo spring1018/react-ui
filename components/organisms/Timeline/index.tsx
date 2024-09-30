00a1be0e7d76483a9c41bf345658717b4977fe61
spring1018 <spring1018nbrandall@gmail.com>
2024-09-30 16:51:24 +0900

    Refactor Timeline component

--- components/organisms/Timeline/index.tsx
+++ components/organisms/Timeline/index.tsx
@@ -6,75 +6,89 @@ import { Dialog } from "@/components/ui/dialog";
 import { useState } from "react";
 
 import {
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
 } from "@/components/ui/dialog";
 
+const DialogWrapper = ({
+	open,
+	setOpen,
+	title,
+	children,
+}: {
+	open: boolean;
+	setOpen: (open: boolean) => void;
+	title: string;
+	children?: React.ReactNode;
+}) => {
+	return (
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogContent onInteractOutside={() => setOpen(false)}>
+				<DialogHeader>
+					<DialogTitle>{title}</DialogTitle>
+					<DialogDescription>{title}</DialogDescription>
+				</DialogHeader>
+				{children ? children : <DialogDescription>foo</DialogDescription>}
+			</DialogContent>
+		</Dialog>
+	);
+};
+
 interface TimelineItemProps {
-  id: string;
-  userName: string;
-  date: Date;
-  tag: string;
-  content: string;
-  DialogContentComponent?: React.ComponentType;
+	id: string;
+	userName: string;
+	date: Date;
+	tag: string;
+	content: string;
+	children?: React.ReactNode;
 }
 
 const TimelineItem = ({
-  id,
-  userName,
-  date,
-  tag,
-  content,
-  DialogContentComponent,
+	id,
+	userName,
+	date,
+	tag,
+	content,
+	children,
 }: TimelineItemProps) => {
-  const [open, setOpen] = useState(false);
-  return (
-    <div>
-      <TimelineItemDialog
-        open={open}
-        setOpen={setOpen}
-        DialogContentComponent={DialogContentComponent}
-      />
-      <div className="flex gap-4 items-center" onClick={() => setOpen(true)}>
-        <Badge>{tag}</Badge>
-        <h2 className="text-sm font-bold">{userName}</h2>
-        <p className="text-sm text-gray-500">{date.toLocaleString()}</p>
-      </div>
-      <ShowMore maxHeight="120px">
-        <div className="flex pl-4 gap-4" onClick={() => setOpen(true)}>
-          <div className="mt-2 border-l-4 border-gray-400" />
-          <Editor initialContent={content} />
-        </div>
-      </ShowMore>
-    </div>
-  );
+	const [open, setOpen] = useState(false);
+
+	return (
+		<div>
+			<DialogWrapper open={open} setOpen={setOpen} title="活動">
+				{children}
+			</DialogWrapper>
+			<div className="flex gap-4 items-center" onClick={() => setOpen(true)}>
+				<Badge>{tag}</Badge>
+				<h2 className="text-sm font-bold">{userName}</h2>
+				<p className="text-sm text-gray-500">{date.toLocaleString()}</p>
+			</div>
+			<ShowMore maxHeight="120px">
+				<div className="flex pl-4 gap-4" onClick={() => setOpen(true)}>
+					<div className="mt-2 border-l-4 border-gray-400" />
+					<Editor initialContent={content} />
+				</div>
+			</ShowMore>
+		</div>
+	);
 };
 
-const TimelineItemDialog = ({ open, setOpen, DialogContentComponent }) => {
-  return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent onInteractOutside={() => setOpen(false)}>
-        <DialogHeader>
-          <DialogTitle>活動</DialogTitle>
-        </DialogHeader>
-        {DialogContentComponent ? (
-          <DialogContentComponent />
-        ) : (
-          <DialogDescription>foo</DialogDescription>
-        )}
-      </DialogContent>
-    </Dialog>
-  );
-};
-
-export const Timeline = ({ items }: { items: TimelineItemProps[] }) => {
-  return (
-    <div className="space-y-4">
-      {items.map((item) => (
-        <TimelineItem key={item.id} {...item} />
-      ))}
-    </div>
-  );
+export const Timeline = ({
+	items,
+	children,
+}: {
+	items: TimelineItemProps[];
+	children?: React.ReactNode;
+}) => {
+	return (
+		<div className="space-y-4">
+			{items.map((item, index) => (
+				<TimelineItem key={item.id} {...item}>
+					{Array.isArray(children) ? children[index] : children}
+				</TimelineItem>
+			))}
+		</div>
+	);
 };
