8c71378581bebaa922b8cda1ebf5a8c39d70a96a
spring1018 <spring1018nbrandall@gmail.com>
2024-10-23 09:50:09 +0900

    Tweak

--- components/molecules/Editor/index.tsx
+++ components/molecules/Editor/index.tsx
@@ -1,26 +1,23 @@
 "use client";
-import { Block, BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useMemo } from "react";
 import "./styles.css";
 
-async function saveToStorage(jsonBlocks: Block[]) {
-	console.log("Saving content:", JSON.stringify(jsonBlocks));
-	console.log("Saving content:", jsonBlocks);
-}
-
 interface EditorProps {
 	mode?: "view" | "edit";
 	initialContent?: string;
 	handleChange?: (content: string) => void;
+	handleBlur?: (content: string) => void;
 }
 
 export default function Editor({
 	mode = "view",
 	initialContent = "",
 	handleChange = () => {},
+	handleBlur = () => {},
 }: EditorProps) {
 	const parsedContent: PartialBlock[] | undefined = useMemo(() => {
 		return initialContent ? JSON.parse(initialContent) : undefined;
@@ -49,10 +46,8 @@ export default function Editor({
 				data-theming-css-variables-demo // ref: https://www.blocknotejs.org/docs/styling-theming/adding-dom-attributes
 				editable={mode === "edit"}
 				onChange={() => handleChange(JSON.stringify(editor.document))}
+				onBlur={() => handleBlur(JSON.stringify(editor.document))}
 			/>
-			{/* {mode === "edit" ? (
-        <Button onClick={() => saveToStorage(editor.document)}>Save</Button>
-      ) : null} */}
 		</div>
 	);
 }
